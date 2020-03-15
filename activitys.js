3114c7cbbdad9a6e4e70d5fad04036964230a88e
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-15 21:19:44 +0000

    initing Aplication - First Commit

---
+++ activitys.js
@@ -0,0 +1,8 @@
+var listElement = document.getElementById('list');
+var inputElement = document.getElementById('input');
+var buttonElement = document.getElementById('button');
+
+var activity = [
+    { text: 'Call to Tamy', Number: '083 XXX-XXXXX'},
+    'Estudy JAvaScript'
+];
