7d0f2a3f2d856c027e7a6194109496bd09d74e1e
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-15 21:34:59 +0000

    Rendering Activitys

--- activitys.js
+++ activitys.js
@@ -2,7 +2,18 @@ var listElement = document.getElementById('list');
 var inputElement = document.getElementById('input');
 var buttonElement = document.getElementById('button');
 
-var activity = [
-    { text: 'Call to Tamy', Number: '083 XXX-XXXXX'},
+var activitys = [
+    'Estudy JAvaScript',
     'Estudy JAvaScript'
-];
+]; 
+
+function renderActivitys () {
+    for (activity of activitys) {
+        var activityElement = document.createElement('li');
+        var activityText = document.createTextNode(activity);
+
+        activityElement.appendChild(activityText);
+        listElement.appendChild(activityElement);
+    }
+}
+renderActivitys();
