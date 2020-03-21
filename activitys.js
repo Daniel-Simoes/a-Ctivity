272eed9dce08fc56fbc4a20bceac6832861dd7e5
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-21 01:13:07 +0000

    Add EventListener to Input

--- activitys.js
+++ activitys.js
@@ -2,50 +2,67 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var activitys = JSON.parse(localStorage.getItem('list_activitys')) || []; 
+var activitys = [
+    'sdsds',
+    'dsffdf',
+    'fdfdg'
+];
 
 function renderActivitys () {
-    listElement.innerHTML = '';
+listElement.innerHTML = '';
 
     for (activity of activitys) {
         var activityElement = document.createElement('li');
         var activityText = document.createTextNode(activity);
+       
+        var linkElement = document.createElement('a');
 
-        var buttonElement = document.createElement('button');
+        
+        
+        var linkText = document.createTextNode(' Done');
+        linkElement.setAttribute('href', '#');
 
         var pos = activitys.indexOf(activity);
-        buttonElement.setAttribute('onclick', 'deleteActivity(' + pos +')');
+        linkElement.setAttribute('onclick', 'deleteActivity (' + pos +')');
 
-        var linkText = document.createTextNode('Done');
-
-        buttonElement.appendChild(linkText);
+        linkElement.appendChild(linkText);
 
         activityElement.appendChild(activityText);
-        activityElement.appendChild( buttonElement);
+        activityElement.appendChild(linkElement);
+
 
         listElement.appendChild(activityElement);
+
+
     }
 }
 
 renderActivitys();
 
 function addActivity() {
-    var activityText = inputElement.value;
-
+var activityText = inputElement.value;
     activitys.push(activityText);
-    inputElement.value = '';
+  inputElement.value = '';
     renderActivitys();
     saveToStorage();
 }
 
 buttonElement.onclick = addActivity;
 
+var input = document.querySelector('#app input');
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.querySelector('#app button').click();
+  }
+});
+
 function deleteActivity(pos) {
     activitys.splice(pos, 1);
-    renderActivitys();
+renderActivitys();
     saveToStorage();
 }
 
 function saveToStorage() {
-    localStorage.setItem('list_activitys',JSON.stringify(activitys));
+   localStorage.setItem('list_activitys',JSON.stringify(activitys));
 }
